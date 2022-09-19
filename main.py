133dd04bc7fc5cbe46d9cc59be09c46a8aab2015
James Shortall <james@Jamess-MacBook-Pro.local>
2022-09-19 18:44:58 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+your_name= input ("What is your name")
+print("wow", your_name, " is a very cool and neat name")
+your_color= input("What is your favorite color")
+print ("wait", your_color, " 1s also ny favorite color")
+your_town = input("Where are you from")
+your_town2 = input("Is your town big")
+your_major = input ("What are you majoring in")
+comment_to_user = f"wow {your_name} comp151 will really help you achieve your {your_major} degree"
+print (comment_to_user)
